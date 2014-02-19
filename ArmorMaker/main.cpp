d8202e71c152f1838a80e274d8ff9e279b961796
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-02-19 08:43:47 -0700

    Update main.cpp
    !! FIKIN KING SPADE MF'r !!

--- ArmorMaker/main.cpp
+++ ArmorMaker/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int Statcnt;
-int a,t,i,sc,gc;
+int a,t,i,j,sc,gc;
 int EntryIdStrt;
 int Tiers;
 int Pclass;
@@ -12,19 +12,18 @@ int Armorlvl;
 int Armor[4]={0,0,0,0};
 int Iclass = 4;
 int Iquality = 4;
-int DispId[12][8]={
+int DispId[12][9]={
 {},
-{},
-{1111,1112,1113,1114,1115,1116,1117,1118},
-{2221,2222,2223,2224,2225,2226,2227,2228},
-{3221,3222,3223,3224,3225,3226,3227,3228},
-{4221,4422,4443,4444,4445,4446,4447,4448},
-{5551,5552,5553,5554,5555,5556,5557,5558},
-{6661,6662,6663,6664,6665,6666,6667,6668},
-{7771,7772,7773,7774,7775,7776,7777,7778},
-{8881,8882,8883,8884,8885,8886,8887,8888},
-{9991,9992,9993,9994,9995,9996,9997,9998},
-{1011,1012,1013,1014,1015,1016,1017,1018}
+{0,1111,1112,1113,1114,1115,1116,1117,1118},
+{0,2221,2222,2223,2224,2225,2226,2227,2228},
+{0,3221,3222,3223,3224,3225,3226,3227,3228},
+{0,4221,4422,4443,4444,4445,4446,4447,4448},
+{0,5551,5552,5553,5554,5555,5556,5557,5558},
+{0,6661,6662,6663,6664,6665,6666,6667,6668},
+{0,7771,7772,7773,7774,7775,7776,7777,7778},
+{0,8881,8882,8883,8884,8885,8886,8887,8888},
+{0,9991,9992,9993,9994,9995,9996,9997,9998},
+{0,1011,1012,1013,1014,1015,1016,1017,1018}
 		};
 int InvData[9]={0,1,3,5,6,7,8,9,10};
 char InvName[9][10]={{""},{"Head"},{"Shoulder"},{"Chest"},{"Waist"},{"Legs"},{"Feet"},{"Wrist"},{"Hands"}};
@@ -35,7 +34,7 @@ int Socket[4]={0,0,0,0};
 int main()
 {
     cout<<"Hello noob!\n";
-    cout<<"Welcome to a stupid little tool.\n";
+    cout<<"Welcome to my stupid little tool.\n\n";
     cout<<"Enter Starting Id for Item entry Id: ";
     cin>>EntryIdStrt;
     cin.ignore();
@@ -45,7 +44,7 @@ int main()
     cout<<"Player Class(1-10): ";
     cin>>Pclass;
     cin.ignore();
-    cout<<"Armor Items Level: ";
+    cout<<"Armor Level: ";
     cin>>Armorlvl;
     cin.ignore();
     cout<<"Armor minimum: ";
@@ -73,6 +72,12 @@ int main()
             StatData[a][4]=StatData[a][3]/Tiers;
             cout<<""<< StatData[a][4] <<"\n";
         };
+            for(j=1;j<=3;j=j+1)
+            {
+            cout<<"Gem "<< j <<" socket type: ";
+            cin>>Socket[j];
+            cin.ignore();
+            }
         ofstream SQLfile;
         SQLfile.open ("Armor.sql");
         SQLfile << "REPLACE INTO `item_template` (`entry`, `class`, `subclass`, `name`, `displayid`, `Quality`, `BuyCount`, `InventoryType`, `AllowableClass`, `ItemLevel`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `armor`, `socketColor_1`, `socketColor_2`, `socketColor_3`) VALUES\n\n";
