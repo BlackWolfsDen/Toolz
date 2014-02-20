ea9e225223d19eaa6df039cd68a3b0321e0465e4
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-02-19 21:15:32 -0700

    Update ArmorBundleBatcher.cpp
    changed item quality from fixed variable to input variable

--- ArmorMaker/ArmorBundleBatcher.cpp
+++ ArmorMaker/ArmorBundleBatcher.cpp
@@ -4,15 +4,10 @@
 
 using namespace std;
 
-int Statcnt;
 int a,t,i,j,sc,gc;
-int EntryIdStrt;
-int Tiers;
-int Pclass;
-int Armorlvl;
-int Armor[4]={0,0,0,0};
+int Statcnt,EntryIdStrt,Tiers,Pclass,Armorlvl,Iquality;
 int Iclass = 4;
-int Iquality = 4;
+int Buycnt = 1;
 int DispId[11][11][9]={//done this way to expand the table 3d for class plotting later
 {{}},//nul no touchy touchy . \/ 8 entries per, 10 groups per, 10 classes. DISPLAY ID'Z
 {{0,0,0,0,0,0,0,0,0},//warrior
@@ -127,11 +122,13 @@ int DispId[11][11][9]={//done this way to expand the table 3d for class plotting
 {0,1011,1012,1013,1014,1015,1016,1017,1018}},
 		};
 int InvData[9]={0,1,3,5,6,7,8,9,10};
-char InvName[9][10]={{""},{"Head"},{"Shoulder"},{"Chest"},{"Waist"},{"Legs"},{"Feet"},{"Wrist"},{"Hands"}};
-char ClassName[11][12]={{""},{"Warrior"},{"Paladin"},{"Hunter"},{"Rogue"},{"Priest"},{"DeathKnight"},{"Shaman"},{"Mage"},{"Warlock"},{"Druid"}};
 int ClassData[11][2]={{0,0},{1,4},{2,4},{4,3},{8,2},{16,1},{32,4},{64,3},{128,1},{256,1},{1024,2}};
 int StatData[11][4]={{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
+int Armor[4]={0,0,0,0};
 int Socket[4]={0,0,0,0};
+char InvName[9][10]={{""},{"Head"},{"Shoulder"},{"Chest"},{"Waist"},{"Legs"},{"Feet"},{"Wrist"},{"Hands"}};
+char ClassName[11][12]={{""},{"Warrior"},{"Paladin"},{"Hunter"},{"Rogue"},{"Priest"},{"DeathKnight"},{"Shaman"},{"Mage"},{"Warlock"},{"Druid"}};
+
 int main()
 {
     cout<<"Hello noob!\n";
@@ -139,75 +136,91 @@ int main()
     cout<<"This will belch out a skeletal structure of armor tiers in an sql file.\n";
     cout<<"fill in the blanks and it it will make a file in working /dir\n";
     cout<<"open the sql edit to fine tune your gearz name and display id. have fun :D\n\n";
+
     cout<<"Enter Starting Id for Item entry Id: ";
     cin>>EntryIdStrt;
     cin.ignore();
+
     cout<<"How many tiers(bundles): ";
     cin>>Tiers;
     cin.ignore();
+
     cout<<"\n(1=Warrior :: 2=Paladin :: 3=Hunter :: 4=Rogue :: 5=Priest)\n";
     cout<<"(6=DK :: 7=Shaman :: 8=Mage :: 9=Warlock :: 10=Druid)\n";
     cout<<"Player Class(1-10): ";
     cin>>Pclass;
     cin.ignore();
+
+    cout<<"Item Quality: ";
+    cin>>Iquality;
+    cin.ignore();
+
     cout<<"Armor Level: ";
     cin>>Armorlvl;
     cin.ignore();
+
     cout<<"Armor minimum: ";
     cin>>Armor[1];
     cin.ignore();
+
      cout<<"Armor Maximum: ";
     cin>>Armor[2];
     cin.ignore();
+
     Armor[3]=(Armor[2]-Armor[1])/(Tiers-1);
     cout<<"How many Stats(max 10): ";
     cin>>Statcnt;
     cin.ignore();
+
          for(a=1;a<=Statcnt;a=a+1)
          {
             cout<<"Class "<< ClassName[Pclass] <<"\n";
             cout<<"Stat "<< a <<" type ID: ";
             cin>>StatData[a][1];
             cin.ignore();
+
             cout<<"Stat "<< a <<" Minimum amount: ";
             cin>>StatData[a][2];
             cin.ignore();
+
             cout<<"Stat "<< a <<" Maximum amount: ";
             cin>>StatData[a][3];
             cin.ignore();
+
             StatData[a][4]=(StatData[a][3]-StatData[a][2])/(Tiers-1);
             cout<<""<< StatData[a][4] <<"\n";
         };
         cout<<"\n1=Meta :: 2=Red :: 4=Yellow :: 8=Blue\n";
+
             for(j=1;j<=3;j=j+1)
-            {
-            cout<<"Gem "<< j <<" socket type: ";
-            cin>>Socket[j];
-            cin.ignore();
-            }
+	            {
+	            cout<<"Gem "<< j <<" socket type: ";
+	            cin>>Socket[j];
+	            cin.ignore();
+	            }
         ofstream SQLfile;
         SQLfile.open ("Armor.sql");
         SQLfile << "REPLACE INTO `item_template` (`entry`, `class`, `subclass`, `name`, `displayid`, `Quality`, `BuyCount`, `InventoryType`, `AllowableClass`, `ItemLevel`, `StatsCount`, `stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, `stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, `stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, `stat_type10`, `stat_value10`, `armor`, `socketColor_1`, `socketColor_2`, `socketColor_3`) VALUES\n\n";
         SQLfile <<"\n-- "<< ClassName[Pclass] <<" ";
             for(t=1;t<=Tiers;t=t+1)
-            {
-                SQLfile <<",\n-- Tier-"<< t <<"\n-- ";
-
-                for(i=1;i<=8;i=i+1)
-                {
-                   SQLfile << ",\n("<< ((EntryIdStrt+((100*t)-100)+i)+((Pclass*10)-(10))) <<", "<< Iclass <<", "<< ClassData[Pclass][1] <<", '"<< ClassName[Pclass] <<"_"<< InvName[i] <<"_T"<< t <<"', "<< DispId[Pclass][t][i] <<", "<< Iquality <<", 1, "<< InvData[i] <<", " << ClassData[Pclass][0] <<", "<< Armorlvl <<", "<< Statcnt <<"";
-                        for(sc=1;sc<=10;sc=sc+1)
-                            {
-                                SQLfile <<", "<< StatData[sc][1] <<", " << (StatData[sc][2]+((StatData[sc][4]*t)-StatData[sc][4])) <<"";
-                            }
-                    SQLfile <<", "<< (Armor[1]+((Armor[3]*t)-Armor[3])) <<"";
-                        for(gc=1;gc<=3;gc=gc+1)
-                            {
-                                SQLfile << ", "<< Socket[gc] <<"";
-                            }
-                            SQLfile << ")";
-                     }
-                }
+	            {
+	                SQLfile <<",\n-- Tier-"<< t <<"\n-- ";
+	
+	                for(i=1;i<=8;i=i+1)
+		                {
+		                   SQLfile << ",\n("<< ((EntryIdStrt+((100*t)-100)+i)+((Pclass*10)-(10))) <<", "<< Iclass <<", "<< ClassData[Pclass][1] <<", '"<< ClassName[Pclass] <<"_"<< InvName[i] <<"_T"<< t <<"', "<< DispId[Pclass][t][i] <<", "<< Iquality <<", "<< Buycnt <<", "<< InvData[i] <<", " << ClassData[Pclass][0] <<", "<< Armorlvl <<", "<< Statcnt <<"";
+		                        for(sc=1;sc<=10;sc=sc+1)
+		                            {
+		                                SQLfile <<", "<< StatData[sc][1] <<", " << (StatData[sc][2]+((StatData[sc][4]*t)-StatData[sc][4])) <<"";
+		                            }
+		                    SQLfile <<", "<< (Armor[1]+((Armor[3]*t)-Armor[3])) <<"";
+		                        for(gc=1;gc<=3;gc=gc+1)
+		                            {
+		                                SQLfile << ", "<< Socket[gc] <<"";
+		                            }
+		                            SQLfile << ")";
+		                     }
+	                }
         SQLfile << ";\n";
         SQLfile.close();
 return 0;
