a309227030250e901ccf45248c453b2f1de96f57
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-02-23 21:47:35 -0700

    Update CreatureStatGen.cpp
    touch-up

--- CreatureStatGen/CreatureStatGen.cpp
+++ CreatureStatGen/CreatureStatGen.cpp
@@ -21,7 +21,7 @@ int main()
 {
     cout<<"Hello noob!\n";
     cout<<"Welcome to my stupid little tool.\n\n";
-    cout<<"This will belch out a Creature level stat values from 80 up to 255 in an sql file.\n";
+    cout<<"This will belch out a Creature level stat values from xxx up to 255 in an sql file.\n";
     cout<<"fill in the blanks and it it will make a file in working /dir\n";
     cout<<"open the sql edit to fine tune then grind it in. have fun :D\n\n";
 
