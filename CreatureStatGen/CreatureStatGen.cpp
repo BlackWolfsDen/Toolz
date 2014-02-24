c8b055e8352611c871e0f4bf046f7464638b0723
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-02-24 06:17:27 -0700

    Update CreatureStatGen.cpp
    REMEBER !! BACKUP !! BACKUP !! BACKUP !!
    for experimenting in creating mobs above level 75 that fight back with more that 1-3 damage and better health.

--- CreatureStatGen/CreatureStatGen.cpp
+++ CreatureStatGen/CreatureStatGen.cpp
@@ -24,7 +24,7 @@ int main()
     cout<<"This will belch out a Creature level stat values from xxx up to 255 in an sql file.\n";
     cout<<"fill in the blanks and it it will make a file in working /dir\n";
     cout<<"open the sql edit to fine tune then grind it in. have fun :D\n\n";
-
+    cout<<"use this to populate the unpopulated levels of creatures for a better lvl 255 experience.\n";
     cout<<"(1=Warrior ::2=Paladin ::4=Rogue ::8=Mage)";
     cout<<"Enter Class Id: ";
     cin>>cclass;
