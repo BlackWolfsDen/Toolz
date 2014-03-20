d9c2e579488bdbe67461a96fff1dfa05a11f9ae7
BlackWolfsDen <blackwolfsden@hotmail.com>
2014-03-20 06:58:44 -0600

    Create player_ex_for_level_80-255.cpp

---
+++ PlayerStatsGenerator/player_ex_for_level_80-255.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int xps,xpf,l;
+
+int main()
+{
+    cout<<"Hello noob!\n";
+    cout<<"slp13at420 of EmuDevs.com here with another stupid lil tool for the stonerz tool box.\n";
+    cout<<"This will belch out a player exp for level values from 80 up to 255 in an sql file.\n";
+    cout<<"fill in the blanks and it it will make a file in working /dir\n";
+    cout<<"open the sql edit to fine tune then grind it in. have fun :D\n\n";
+    cout<<"use this to populate the unpopulated levels of needed experience for a better lvl 255 experience.\n";
+ 
+    cout<<"\n\n lvl 80 Experience start value: ";
+    cin>>xps;
+    cin.ignore();
+
+    cout<<"\n lvl 255 Experiance end value: ";
+    cin>>xpf;
+    cin.ignore();
+    int xpr=xps;
+
+    ofstream SQLfile;
+    SQLfile.open ("player_xp_for_level_80-255.sql");
+    SQLfile << "REPLACE INTO `player_xp_for_level` (`lvl`, `xp_for_next_level`) VALUES\n";
+    SQLfile <<"\n-- ";
+            for(l=80;l<=255;l=l+1)
+            {
+                 SQLfile << ",\n("<< l <<", "<< xpr <<")";
+                 xpr=xpr+((xpf-xps)/(255-80));
+
+            }
+    SQLfile << ";\n";
+    SQLfile.close();
+
+return 0;
+
+}
+
