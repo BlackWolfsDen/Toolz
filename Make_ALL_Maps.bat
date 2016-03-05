2687b41a8dc8441e7ec9ca7212ef1ed60b7cd0d0
Grumbo <blackwolfsden@hotmail.com>
2016-03-05 13:36:44 -0700

    Update Make_ALL_Maps.bat

--- Make_ALL_Maps.bat
+++ Make_ALL_Maps.bat
@@ -1,3 +1,6 @@
+rem Does ALL the map, vmaps and mmaps without you having run each map tool
+rem i just start it in the evening and find everything done in the morning :D
+
 mapextractor.exe
 
 vmap4extractor.exe
